41ce4cbbc95dbc8b94785a6c2b0cd24467cdfba6
Diego <148961694+DiegoCaballeroRuiz@users.noreply.github.com>
2026-02-20 18:27:22 +0100

    Camara?

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -3,17 +3,25 @@ extends CharacterBody3D
 signal startMoving
 signal stopMoving
 
-@export var speed = 50.0
-@export var sensibility = 0.01
+@export var speed := 50.0
+@export var horizontal_sensitivity := 0.01
+@export var vertical_sensitivity := 0.005
+
+@export var min_rotation := -50
+@export var max_rotation := 50
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	rotation.x = 0
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		event = event as InputEventMouseMotion
+		rotate_y(-event.relative.x * horizontal_sensitivity)
 		
-		rotate_y(-event.relative.x * sensibility)
+		rotate_object_local(Vector3.RIGHT, event.relative.y * vertical_sensitivity)
+		rotation.x = clamp(rotation.x, deg_to_rad(min_rotation), deg_to_rad(max_rotation))
+		
 	elif event.is_action_pressed("Moving"):
 		startMoving.emit()
 	elif event.is_action_released("Moving"):
