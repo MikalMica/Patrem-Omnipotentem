092409a57e7beec0d0f0a84b101e43b08ac25936
Diego <148961694+DiegoCaballeroRuiz@users.noreply.github.com>
2026-02-20 21:04:24 +0100

    Interactuar con el dialogo funciona

--- Scripts/dialog_interactable.gd
+++ Scripts/dialog_interactable.gd
@@ -2,19 +2,25 @@ extends Area3D
 
 @export var outline : Sprite3D 
 @export var dialog : DialogueResource
+@export var interact_canvas : TextFadeIn
 
 var player_is_near : bool
 	
 func _ready() -> void:
-	body_entered.connect(on_body_change, true)
-	body_exited.connect(on_body_change, false)
+	body_entered.connect(func(body : Node3D): on_body_change(body, true))
+	body_exited.connect(func(body : Node3D): on_body_change(body, false))
 
 func on_body_change(body : Node3D, entered : bool) -> void: 
 	if(body is not CharacterBody3D): 
-		pass
+		return
 		
+	print("Entered is ", entered)
 	player_is_near = entered
 	outline.visible = entered
+	
+	interact_canvas.visible = entered
+	if(entered):
+		interact_canvas.fade_in()
 
 func _input(event: InputEvent) -> void:
 	if(player_is_near and event.is_action_pressed("Interact")):
---
+++ Scripts/text_fade_in.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+class_name TextFadeIn
+
+@export var animation_player : AnimationPlayer
+
+func _ready() -> void:
+	visible = false
+	
+func fade_in() -> void:
+	animation_player.play("fade_in")
