75983ddbe8351ca70d1f58a11f7d7a05fc1921b9
Javier Valseca <javivals@ucm.es>
2026-02-21 22:40:24 +0100

    socket attack animation muy guapa

--- Scripts/socket.gd
+++ Scripts/socket.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
 @export var wire:TextureRect
-@export var head:Sprite2D
-var extending:bool = true
+@export var head:Node2D
 
 @export var target:Node2D
 
 func _ready() -> void:
 	var center:Vector2 = get_tree().get_first_node_in_group(&"CENTER").global_position
-	global_position.y = center.y
+	
+	look_at(center)
+	
+	var distance = global_position.distance_to(target.global_position)
 	target.global_position = center
+	target.look_at(global_position)
 	
 	await get_tree().create_timer(1).timeout
-	
 	var tween:Tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_IN)
 	tween.set_trans(Tween.TRANS_EXPO)
 	tween.tween_property(head, "global_position", target.global_position, 0.5)
-
-func _process(_delta: float) -> void:
-	if not extending: return
-	extend()
 	
-func extend():
-	wire.size.x = head.position.x
+	var o_tween:Tween = get_tree().create_tween()
+	o_tween.set_ease(Tween.EASE_IN)
+	o_tween.set_trans(Tween.TRANS_EXPO)
+	o_tween.tween_property(wire, "size:x", distance, 0.5)
