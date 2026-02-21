6c56447f5b8fc3489be82df222ae15524019e6b1
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-02-21 13:59:07 +0100

    timer

--- Scripts/default_obstacle.gd
+++ Scripts/default_obstacle.gd
@@ -3,14 +3,16 @@ extends IAttack
 @export var radius = 50
 @export var timeBetweenAttacks = 1.0
 var obstacle = preload("res://Scenes/Objects/attack_placeholder.tscn")
-@onready var timer = Timer.new()
 
 func _attack() -> void:
+	_spawn_recursive()
+
+func _spawn_recursive() -> void:
 	var obs = obstacle.instantiate() as DirSeeker
 	var randAng = randf() * (2 * PI)
 	var pos = Vector2(cos(randAng) * radius, sin(randAng) * radius)
 	obs.position = pos
 	obs.set_direction(position - pos)
 	add_child(obs)
-	timer.start(timeBetweenAttacks)
+	var timer = get_tree().create_timer(timeBetweenAttacks)
 	timer.timeout.connect(_attack)
