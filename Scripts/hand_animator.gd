db14a1867024f775cf5c10304ad72a31a67adbf7
Erisolo <marsolor@ucm.es>
2026-02-22 00:12:12 +0100

    mainscene y por de tonterias mas

--- Scripts/hand_animator.gd
+++ Scripts/hand_animator.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 class_name HandAnimator
 
+
 @export var anim_player : AnimationPlayer
 
 func _ready() -> void:
@@ -13,4 +14,6 @@ func transition_hand() -> void:
 func papear() -> void:
 	self.visible = true
 	anim_player.play("papear")
+	$AudioStreamPlayer.pitch_scale = randf_range(0.9, 1.1);
+	$AudioStreamPlayer.play();
 	anim_player.animation_finished.connect(func(_any) -> void: self.visible = false)
