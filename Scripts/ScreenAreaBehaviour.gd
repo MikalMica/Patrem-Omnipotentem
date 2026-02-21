4e7382097f4c5af43185d89c16a3b8bb8f985882
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-02-22 00:01:03 +0100

    areas finiquitadas

--- Scripts/ScreenAreaBehaviour.gd
+++ Scripts/ScreenAreaBehaviour.gd
@@ -4,17 +4,17 @@ class_name ScreenArea
 @export var modules: Array[PackedScene]
 @export var fadeInTime: float = 1.0
 @export var areaLifeTime: float = 3.0
-var areaOffset: float = 50.
-var dirs = [-1, 0, 1]
+@export var areaModulesExt: int = 4
+@export var areaSize: float = 300.
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print(global_position)
-	for i in range(0, 3):
-		for j in range(0, 3):
+	for i in range(0, areaModulesExt):
+		for j in range(0, areaModulesExt):
 			if randi() % 2 == 1:
 				var mod = modules[randi() % modules.size()].instantiate() as FadeInArea
-				mod.position = Vector2(dirs[i] * areaOffset, dirs[j] * areaOffset)
+				mod.position = Vector2((areaSize/areaModulesExt) * j, (areaSize/areaModulesExt) * i)
 				mod.setSpeed(fadeInTime)
 				add_child(mod)
 				mod._playAnim()
