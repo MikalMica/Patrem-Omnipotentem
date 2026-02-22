787ce41310d7dde4a17897f90089dfa0cde88095
Javier Valseca <javivals@ucm.es>
2026-02-22 22:09:03 +0100

    asfc

--- Scripts/billboarding.gd
+++ Scripts/billboarding.gd
@@ -2,9 +2,10 @@ extends Node3D
 
 @export var player : CharacterBody3D
 @export var y_billboard = false
+@export var inverse = false
 
 func _process(_delta : float) -> void:
-	look_at(player.global_position)
+	look_at(player.global_position, Vector3.UP, inverse)
 	if not y_billboard:
 		rotation.x = .0
 		rotation.z = .0
