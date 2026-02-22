267f5e2f46ec9d3422785de3ad9d787b50bccade
Javier Valseca <javivals@ucm.es>
2026-02-22 21:51:55 +0100

    Mario Vaquerizo

--- Scripts/billboarding.gd
+++ Scripts/billboarding.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 @export var player : CharacterBody3D
+@export var y_billboard = false
 
 func _process(_delta : float) -> void:
 	look_at(player.global_position)
-	rotation.x = .0
-	rotation.z = .0
+	if not y_billboard:
+		rotation.x = .0
+		rotation.z = .0
