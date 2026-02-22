2fa8cd1ae44226b8db2b6e207fc4fa955e1e1830
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2026-02-22 17:23:59 +0100

    triggers

--- Scripts/triggerScene.gd
+++ Scripts/triggerScene.gd
@@ -2,6 +2,9 @@ extends Area3D
 
 @export var ScenePath: String
 
+func _ready() -> void:
+	body_entered.connect(_body_entered)
+
 func _body_entered(body: Node3D):
 	if body is not CharacterBody3D:
 		return
