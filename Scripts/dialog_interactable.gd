78d6e82a4f2dca02cd143caa498e7b5b39838b7f
Erisolo <marsolor@ucm.es>
2026-02-22 03:50:07 +0100

    shrek

--- Scripts/dialog_interactable.gd
+++ Scripts/dialog_interactable.gd
@@ -20,12 +20,14 @@ func on_body_change(body : Node3D, entered : bool) -> void:
 	player_is_near = entered
 	outline.visible = entered
 	
-	interact_canvas.visible = entered
-	if(entered):
-		interact_canvas.fade_in()
+	if (interact_canvas != null):
+		interact_canvas.visible = entered
+		if(entered):
+			interact_canvas.fade_in()
 
 func _input(event: InputEvent) -> void:
 	if(player_is_near and event.is_action_pressed("Interact") and not in_dialogue):
 		in_dialogue = true
 		DialogueManager.show_dialogue_balloon(dialog, "start")
-		interact_canvas.visible = false
+		if(interact_canvas != null):
+			interact_canvas.visible = false
