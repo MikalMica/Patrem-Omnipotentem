d73409be1d00e91a438f42260bc915d7d532a8e3
Javier Valseca <javivals@ucm.es>
2026-02-27 15:17:02 +0100

    arreglado collider invisible

--- Scripts/trail.gd
+++ Scripts/trail.gd
@@ -7,21 +7,24 @@ class_name trail
 var links: Array[Node2D]
 var lastLink: Area2D
 var timeBetweenDespawns = .2
+var firstLink:Node2D
 
 signal trailDespawned
 
 func _ready() -> void:
 	(bulletArea.get_parent() as FollowObstacle).waiting.connect(_despawnTrail)
 	bulletArea.area_exited.connect(_spawnTrail)
-	var mLink = link.instantiate() as Node2D
-	mLink.global_rotation = bulletArea.global_rotation + (PI/2)
-	mLink.global_position = bulletArea.global_position
-	mLink.visible = false
-	add_child(mLink)
-	lastLink = mLink as Area2D
+	firstLink = link.instantiate() as Node2D
+	firstLink.global_rotation = bulletArea.global_rotation + (PI/2)
+	firstLink.global_position = bulletArea.global_position
+	add_child(firstLink)
+	firstLink.hide()
+	firstLink.monitoring = false
+	lastLink = firstLink as Area2D
 
 func _spawnTrail(area: Area2D) -> void:
 	if area == lastLink:
+		if firstLink: firstLink.queue_free()
 		var mLink = link.instantiate() as Node2D
 		add_child.call_deferred(mLink)
 		mLink.global_position = bulletArea.global_position
